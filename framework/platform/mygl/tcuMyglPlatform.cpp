78699043db669615f6f08161df55e5a45953e1c5
Michael Fairley <michaelfairley@gmail.com>
2017-10-25 18:15:54 -0400

    Use EGL context factory

--- framework/platform/mygl/tcuMyglPlatform.cpp
+++ framework/platform/mygl/tcuMyglPlatform.cpp
@@ -1,56 +1,13 @@
 #include "tcuMyglPlatform.hpp"
 
-#include "gluRenderContext.hpp"
-#include "gluContextFactory.hpp"
-#include "gluRenderConfig.hpp"
 #include "glwFunctions.hpp"
 #include "tcuRenderTarget.hpp"
 #include "egluNativeDisplay.hpp"
 #include "eglwLibrary.hpp"
 #include "eglwEnums.hpp"
+#include "egluGLContextFactory.hpp"
 
 namespace tcu {
-  class MyglRenderContext: public glu::RenderContext {
-  public:
-    MyglRenderContext(const glu::RenderConfig& config)
-      : type(config.type)
-      , renderTarget (0, 0, tcu::PixelFormat(0,0,0,0), 0, 0, 0) {
-
-    }
-
-    ~MyglRenderContext() {}
-
-    const glw::Functions& getFunctions() const {
-      return functions;
-    }
-
-    const tcu::RenderTarget& getRenderTarget() const {
-      return renderTarget;
-    }
-
-    void postIterate() {}
-
-    glu::ContextType getType() const {
-      return type;
-    }
-
-  private:
-    const glu::ContextType type;
-    glw::Functions functions;
-    tcu::RenderTarget renderTarget;
-  };
-
-  class MyglContextFactory: public glu::ContextFactory {
-  public:
-    MyglContextFactory() : glu::ContextFactory("mygl", "Mygl Context") {}
-
-    glu::RenderContext* createContext(const glu::RenderConfig& config,
-                                      const tcu::CommandLine& cmdLine,
-                                      const glu::RenderContext* sharedContext) const {
-      return new MyglRenderContext(config);
-    }
-  };
-
   class MyglDisplay: public eglu::NativeDisplay {
   public:
     MyglDisplay(): eglu::NativeDisplay(CAPABILITY_GET_DISPLAY_LEGACY),
@@ -81,8 +38,8 @@ namespace tcu {
 
 
   MyglPlatform::MyglPlatform() {
-    m_contextFactoryRegistry.registerFactory(new MyglContextFactory());
     m_nativeDisplayFactoryRegistry.registerFactory(new MyglDisplayFactory());
+    m_contextFactoryRegistry.registerFactory(new eglu::GLContextFactory(m_nativeDisplayFactoryRegistry));
   }
 
   MyglPlatform::~MyglPlatform() {}
